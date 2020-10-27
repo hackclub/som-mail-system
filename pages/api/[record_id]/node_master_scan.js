9e4cd832571f2aeb2fa0883038d8f403f7db6348
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-10-27 20:53:46 +0800

    thanks git, jk no love for you

--- pages/api/[record_id]/node_master_scan.js
+++ pages/api/[record_id]/node_master_scan.js
@@ -2,25 +2,27 @@
 // "Node Master Scanned At" field with the current time
 
 export async function setNodeMasterScan(record_id) {
-  const endpoint = "https://api2.hackclub.com/v0.1/SOM Sticker Requests/Sticker Requests"
+  const endpoint = `https://api2.hackclub.com/v0.1/SOM Sticker Requests/Sticker Requests?authKey=${process.env.AIRBRIDGE_TOKEN}`
   const rawRecords = await fetch(endpoint, {
-    method: "PATCH",
-    body: {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
       id: record_id,
       fields: {
-        "Node Master Scanned At": Date.now()
+        'Node Master Scanned At': Date.now()
       }
-    }
+    })
   })
-  const parsedRecords = await rawRecords.json()
-  const record = parsedRecords[0]
+  const record = await rawRecords.json()
   return record
 }
 
 export default async (req, res) => {
   const { record_id } = req.query
   if (!record_id || record_id === undefined) {
-    return res.status(404).json({ status: 404, error: "missing record ID" })
+    return res.status(404).json({ status: 404, error: 'missing record ID' })
   }
 
   const record = await setNodeMasterScan(record_id)
