bf0a824f0639aae58e57857db2e89171cedce5d7
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-10-27 08:54:39 +0800

    Update recipient_scan.js

--- pages/api/[record_id]/recipient_scan.js
+++ pages/api/[record_id]/recipient_scan.js
@@ -1,6 +1,23 @@
 // This should input a "record_id", & update the airtable record's
 // "Recipient Scanned At" field with the current time
 
+export async function setRecipientScan(record_id) {
+  const endpoint = `https://api2.hackclub.com/v0.1/SOM Sticker Requests/Sticker Requests?authKey=${process.env.AIRBRIDGE_TOKEN}`
+  const rawRecords = await fetch(endpoint, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      id: record_id,
+      fields: {
+        "Recipient Scanned At": Date.now()
+      }
+    })
+  })
+  const record = await rawRecords.json()
+  return record
+}
 
 export default async (req, res) => {
   const { record_id } = req.query
@@ -9,4 +26,6 @@ export default async (req, res) => {
   }
 
   const record = await setRecipientScan(record_id)
+
+  res.json(record)
 }
