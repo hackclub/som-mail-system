08891c7b18767bf17d5c65d6d15da468a059cb11
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-10-28 08:52:49 +0800

    Finish Off the map

--- components/map.js
+++ components/map.js
@@ -1,63 +1,71 @@
-import React, { useEffect, useState } from "react";
-import { csv } from "d3-fetch";
-import { scaleLinear } from "d3-scale";
+import React, { useEffect, useState } from 'react'
+import { csv } from 'd3-fetch'
+import { scaleLinear } from 'd3-scale'
 import {
   ComposableMap,
   Geographies,
   Geography,
   Sphere,
   Graticule
-} from "react-simple-maps";
-var Rainbow = require('rainbowvis.js');
-var myRainbow = new Rainbow();
-myRainbow.setSpectrum('#ff8c37', '#ec3750');
+} from 'react-simple-maps'
+var Rainbow = require('rainbowvis.js')
+var myRainbow = new Rainbow()
+myRainbow.setSpectrum('#ff8c37', '#ec3750')
 
 const geoUrl =
-  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+  'https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json'
 
 const colorScale = scaleLinear()
   .domain([0.29, 0.68])
-  .range(["#ffedea", "#ff5233"]);
+  .range(['#ffedea', '#ff5233'])
 
-const MapChart = (props) => {
-  const [data, setData] = useState([]);
+const MapChart = props => {
+  const [data, setData] = useState([])
 
   useEffect(() => {
-      setData(props.mapData);
-      console.log(props.mapData)
-  }, []);
+    setData(props.mapData)
+    console.log(props.mapData)
+  }, [])
 
   return (
     <ComposableMap
       projectionConfig={{
         rotate: [-10, 0, 0],
-        scale: 147
+        scale: 160
       }}
-      style={{width: '100%', marginTop: '-72px'}}
+      style={{ width: '100%' }}
     >
       <Sphere stroke="#E4E5E6" strokeWidth={0.5} />
       <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
       {data.length > 0 && (
         <Geographies geography={geoUrl}>
           {({ geographies }) =>
-            geographies.map((geo) => {
-              const d = data.find((s) => s.ISO3 === geo.properties.ISO_A3);
-              
-              if(d){console.log((d['Length']*10000))
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill={'#'+myRainbow.colourAt(d['Length']*1500)}
-                />
-              );}
+            geographies.map(geo => {
+              const d = data.find(s => s.ISO3 === geo.properties.ISO_A3)
+              if (d) {
+                console.log(d['Length'] * 10000)
+                return (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    fill={'#' + myRainbow.colourAt(d['Length'] * 1500)}
+                  />
+                )
+              } else {
+                return (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    fill={'lightgrey'}
+                  />
+                )
+              }
             })
           }
         </Geographies>
       )}
     </ComposableMap>
-  );
-};
+  )
+}
 
-export default MapChart;
-
+export default MapChart
--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,74 @@
 import { Container, Heading, Grid, Card } from 'theme-ui'
 import MapChart from '../components/map'
 import { groupBy } from 'lodash'
-var countries = require("i18n-iso-countries");
+var countries = require('i18n-iso-countries')
 
 export default function HomePage(props) {
   return (
-      <Container sx={{
+    <Container
+      sx={{
         paddingTop: '60px',
         paddingBottom: '60px',
-        textAlign: 'center',
-        width: '90%',
-      }}>
-        <Heading>Mission Impossible:</Heading>
-        <Heading as="h1" sx={{ fontSize: '4em', paddingTop: '10px', paddingBottom: '20px'}}>Summer of Making Stickers</Heading>
-        <Grid
-          columns={[null, 2, 2]}
-          gap={3}
-          sx={{ code: { marginTop: '20px', fontSize: 0 } }}
-        >
+        textAlign: 'left',
+        width: '90%'
+      }}
+    >
+      <Grid
+        columns={[null, null,null, '1fr 2fr']}
+        gap={5}
+        sx={{ code: { marginTop: '20px', fontSize: 0 } }}
+      >
         <div>
-        <Card>Hi</Card>
-        <Card sx={{ code: { marginTop: '20px'} }}>Hi</Card>
+          <Heading>Mission Impossible:</Heading>
+          <Heading
+            as="h1"
+            sx={{ fontSize: '3em', paddingTop: '10px', paddingBottom: '20px' }}
+          >
+            Summer of Making Stickers
+          </Heading>
+          <Card>Hi</Card>
+          <Card sx={{ marginTop: '20px' }}>Hi</Card>
         </div>
-        <MapChart style={{width: '100%'}} mapData={props.data} />
-        </Grid>
-      </Container>
+        <Container sx={{marginTop: ['-72px']}}>
+          <MapChart style={{ width: '100%' }} mapData={props.data} />
+        </Container>
+      </Grid>
+    </Container>
   )
 }
 
-export async function getStaticProps(){
+export async function getStaticProps() {
   let data = await fetch(
-    `https://api2.hackclub.com/v0.1/SOM%20Sticker%20Requests/Sticker%20Requests?authKey=${process.env.AIRBRIDGE_TOKEN}`)
-    .then(r => r.json())
-    .then((data) =>
-    data.map(({ id, fields }) => ({
-      id,
-      ISO3: fields["Country Dropdown"] ? countries.alpha2ToAlpha3(fields["Country Dropdown"].replace('Tanzania', '(TZ)').replace('Korea (the Republic of)', 'South Korea').replace('(the former Yugoslav Republic of)','').match(/\(([^)]+)\)/)[1]) : 'Unknown', 
-      Country: fields["Country Dropdown"]
-    }))
+    `https://api2.hackclub.com/v0.1/SOM%20Sticker%20Requests/Sticker%20Requests?authKey=${process.env.AIRBRIDGE_TOKEN}`
   )
+    .then(r => r.json())
+    .then(data =>
+      data.map(({ id, fields }) => ({
+        id,
+        ISO3: fields['Country Dropdown']
+          ? countries.alpha2ToAlpha3(
+              fields['Country Dropdown']
+                .replace('Tanzania', '(TZ)')
+                .replace('Korea (the Republic of)', 'South Korea')
+                .replace('(the former Yugoslav Republic of)', '')
+                .match(/\(([^)]+)\)/)[1]
+            )
+          : 'Unknown',
+        Country: fields['Country Dropdown']
+      }))
+    )
   const massiveLength = data.length
   data = groupBy(data, 'ISO3')
   let finalData = []
   for (var key in data) {
     if (data.hasOwnProperty(key)) {
-      let x;
-      finalData.push({'ISO3': data[key][0]['ISO3'], 'Country': data[key][0]['Country'], 'Length': data[key].length/ massiveLength})} 
+      let x
+      finalData.push({
+        ISO3: data[key][0]['ISO3'],
+        Country: data[key][0]['Country'],
+        Length: data[key].length / massiveLength
+      })
+    }
   }
-  return {props: {data: finalData}}
+  return { props: { data: finalData, totalAmount: data.length} }
 }
