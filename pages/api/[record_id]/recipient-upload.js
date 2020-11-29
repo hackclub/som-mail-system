cfc7edcaa30dd3a3db2c1ecb1429b5674016a1d0
Max Wofford <max@maxwofford.com>
2020-11-29 10:29:11 -0500

    Upload to CDN before posting to Airtable

--- pages/api/[record_id]/recipient-upload.js
+++ pages/api/[record_id]/recipient-upload.js
@@ -22,7 +22,19 @@ async function postInSlack({channel, messageText, attachImageByUrl, thread}) {
   })
 }
 
-export async function setRecipientUpload(record_id, image_url) {
+async function imgToCDN(url) {
+  const upload = await fetch('https://cdn.hackclub.com/api/v1/new', {
+    method: 'POST',
+    body: JSON.stringify([url])
+  }).then(d => d.json())
+  .catch(err => {
+    console.error(err)
+  })
+  return upload[0]
+}
+
+export async function setRecipientUpload(record_id, tmp_image_url) {
+  const cdn_url = await imgToCDN(tmp_image_url)
   const endpoint = `https://api2.hackclub.com/v0.1/SOM%20Sticker%20Requests/Sticker%20Requests?authKey=${process.env.AIRBRIDGE_TOKEN}`
   const rawRecords = await fetch(endpoint, {
     method: 'PATCH',
@@ -34,7 +46,7 @@ export async function setRecipientUpload(record_id, image_url) {
       fields: {
         'Recipient Uploads': [
           {
-            url: image_url
+            url: cdn_url
           }
         ]
       }
